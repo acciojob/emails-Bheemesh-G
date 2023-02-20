510b64b4473ffba22912539b6bc49db114663a6d
Bheemesh <gbheemesh888@gmail.com>
2023-02-20 09:01:16 +0530

    Done8

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -13,8 +13,8 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-        super(emailId);
-       this.inboxCapacity = Integer.MAX_VALUE;
+        super(emailId,Integer.MAX_VALUE);
+       //this.inboxCapacity = Integer.MAX_VALUE;
        calendar = new ArrayList<>();
 
     }
